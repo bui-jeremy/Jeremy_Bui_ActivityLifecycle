30eb6380aa407e54d430865bb18546b002db2c22
bui-jeremy <jeremybui2002@gmail.com>
2024-02-07 17:31:59 -0500

    "Logged onCreate()"

---
+++ app/src/main/java/com/bignerdranch/android/jeremybui_activitylifecycle/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.bignerdranch.android.jeremybui_activitylifecycle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.d("ActivityLifecycle", "onCreate called")
+    }
+}
