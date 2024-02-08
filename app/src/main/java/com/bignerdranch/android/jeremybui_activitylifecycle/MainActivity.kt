7a470b78c0f5e185e0c16b6c2345c6d1fd97af8c
bui-jeremy <jeremybui2002@gmail.com>
2024-02-07 22:14:32 -0500

    Added other events to logger

--- app/src/main/java/com/bignerdranch/android/jeremybui_activitylifecycle/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/jeremybui_activitylifecycle/MainActivity.kt
@@ -11,4 +11,34 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         Log.d("ActivityLifecycle", "onCreate called")
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("ActivityLifecycle", "onStart called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("ActivityLifecycle", "onResume called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("ActivityLifecycle", "onPause called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("ActivityLifecycle", "onStop called")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d("ActivityLifecycle", "onRestart called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("ActivityLifecycle", "onDestroy called")
+    }
 }
