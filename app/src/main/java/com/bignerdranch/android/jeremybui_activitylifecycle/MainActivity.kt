6d29285856d719a906c2e754deaa7b79872cc772
bui-jeremy <jeremybui2002@gmail.com>
2024-02-08 17:57:24 -0500

    Buttons for text view and edit text added

--- app/src/main/java/com/bignerdranch/android/jeremybui_activitylifecycle/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/jeremybui_activitylifecycle/MainActivity.kt
@@ -3,12 +3,21 @@ package com.bignerdranch.android.jeremybui_activitylifecycle
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-
-
+import android.widget.Button
+import android.widget.TextView
+import android.widget.EditText
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val helloButton: Button = findViewById(R.id.btnHello)
+        val helloTV: TextView = findViewById(R.id.TVHello)
+        val helloET: EditText = findViewById(R.id.ETVHello)
+
+        helloButton.setOnClickListener{
+            helloTV.text = "Hello"
+            helloET.setText("Hello")
+        }
         Log.d("ActivityLifecycle", "onCreate called")
     }
 
